55ed29f503e779d304835f231d8153d03acec424
Tim Jarzombek <tim@jarz.net>
2018-02-27 21:23:29 -0500

    Remove connection info from WwiContext

--- Database/Models/WwiContext.cs
+++ Database/Models/WwiContext.cs
@@ -38,15 +38,6 @@ namespace WideWorldImporters.Database.Models
         public virtual DbSet<TransactionTypes> TransactionTypes { get; set; }
         public virtual DbSet<VehicleTemperatures> VehicleTemperatures { get; set; }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            if (!optionsBuilder.IsConfigured)
-            {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer(@"Server=syno.home.jarz.net;Database=WideWorldImporters;User Id=sa;Password=Sql!Server123");
-            }
-        }
-
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<BuyingGroups>(entity =>
